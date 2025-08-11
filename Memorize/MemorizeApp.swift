8f5df0c20a70408aa633dea978461c5cec1c88a7
Sean Joyce <seanthomasjoyce@icloud.com>
2025-08-11 18:54:33 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sean Joyce on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
